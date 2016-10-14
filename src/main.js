922cd9c84903faeca80cf80e88d28d328429a658
leviathan88 <levigithuber@gmail.com>
2016-10-14 23:06:45 +0200

    creating src folder and main.tsx

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+var App = function () { return React.createElement("h1", null, "dont look back"); };
+render(React.createElement(App, null), document.getElementById('app'));
+//# sourceMappingURL=main.js.map
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const App = () => <h1>dont look back</h1>
+
+render(<App />, document.getElementById('app'))
